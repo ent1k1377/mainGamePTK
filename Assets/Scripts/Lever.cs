33988c9402cd75876555a26a1c1cdfe7ed6d92e3
ent1k1337 <ent1k1337@yandex.ru>
2020-11-14 00:12:49 +0300

    Добавил рычаг, исправил баг с прыжком по стене

---
+++ Assets/Scripts/Lever.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    public bool leftTurned = true;
+    public Sprite LTL;
+    public Sprite LTR;
+    public Animator animBoard;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (leftTurned)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = LTL;
+            animBoard.SetBool("onBoard", false);
+        }
+        else
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = LTR;
+            animBoard.SetBool("onBoard", true);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "sword")
+        {
+            leftTurned = !leftTurned;
+        }
+    }
+}
