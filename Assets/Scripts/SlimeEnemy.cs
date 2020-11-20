504adb538902af644cdd5778904715b0aade0546
ent1k1337 <ent1k1337@yandex.ru>
2020-11-20 23:37:52 +0300

    Добавлен враг(Слайм)

---
+++ Assets/Scripts/SlimeEnemy.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeEnemy : MonoBehaviour
+{
+    public float leftBorder;
+    public float rightBorder;
+
+    public float speed = 0.2f;
+    public int moveRight = -1;
+    Vector2 pos;
+
+    Animator anim;
+    public bool death = false;
+
+    public HeroMove HM;
+
+
+    void Start()
+    {
+        gameObject.transform.localScale = new Vector2(moveRight, 1);
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (!death)
+        {
+            if (col.tag == "sword")
+            {
+                death = true;
+            }
+            else if (col.tag == "pers")
+            {
+                HM.CheckHealth();
+            }
+        }
+
+    }
+
+    void Move()
+    {
+        if (death)
+        {
+            anim.SetBool("onDeath", death);
+            Destroy(gameObject, 1f);
+            return;
+        }
+
+
+        pos = gameObject.GetComponent<Transform>().position;
+        print(123);
+        if (pos.x > rightBorder)
+        {
+            speed = -Math.Abs(speed);
+            gameObject.transform.localScale = new Vector2(1, 1);
+        }
+        else if (pos.x < leftBorder)
+        {
+            speed = Math.Abs(speed);
+            gameObject.transform.localScale = new Vector2(-1, 1);
+        }
+
+        gameObject.GetComponent<Transform>().position = new Vector2(pos.x + speed * Time.deltaTime, pos.y);
+    }
+
+
+
+
+}
