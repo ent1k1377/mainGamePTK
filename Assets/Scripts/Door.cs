d39930ef763bdd78036144fb7e89a3139148a916
ent1k1337 <ent1k1337@yandex.ru>
2020-11-23 00:16:28 +0300

    Добавлена дверь, а также главное меню

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+    public int indexScene;
+    public Animator animHero;
+    public HeroMove HM;
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        print(1);
+        if (col.tag == "pers")
+        {
+            print(2);
+            CheckPressedButton();
+        }
+    }
+
+    void CheckPressedButton()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            StartCoroutine(LoadScene());
+        }
+    }
+    IEnumerator LoadScene()
+    {
+        HM.enabled = false;
+        animHero.SetBool("onDeath", true);
+        animHero.SetBool("onDeathAstral", false);
+        animHero.Play("hero_death");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(indexScene);
+    }
+}
--- Assets/Scripts/HeroTracking.cs
+++ Assets/Scripts/HeroTracking.cs
@@ -6,10 +6,11 @@ public class HeroTracking : MonoBehaviour
 {
     public GameObject pers;
     Transform pers_trans;
-    Vector2 pers_pos;
     Vector2 cam_pos;
     public float speed_lerp = 2f;
-    // Start is called before the first frame update
+
+    public float[] borders = new float[2]; // elem1 - left, elem2 - rigth
+
     void Start()
     {
         pers_trans = pers.GetComponent<Transform>();
@@ -18,14 +19,30 @@ public class HeroTracking : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Move();
+        CheckBorder();
+    }
 
-
+    void Move()
+    {
         transform.position = Vector2.Lerp(transform.position, pers_trans.position, speed_lerp * Time.deltaTime);
         cam_pos = transform.position;
         if (-8 >= transform.position.x)
         {
             cam_pos.x = -8;
-            
+
+        }
+    }
+
+    void CheckBorder()
+    {
+        if (borders[0] >= transform.position.x)
+        {
+            transform.position = new Vector2(borders[0], transform.position.y);
+        }
+        if (borders[1] <= transform.position.x)
+        {
+            transform.position = new Vector2(borders[1], transform.position.y);
         }
     }
 }
