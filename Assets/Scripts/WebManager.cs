20fbbff18c0db1c7585fb84fab576a030a7da877
ent1k1337 <ent1k1337@yandex.ru>
2020-12-05 17:21:40 +0300

    Подправлены звуки

---
+++ Assets/Scripts/WebManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebManager : MonoBehaviour
+{
+
+    static public IEnumerator ConnectDB(int id, int coin, int passge, int death)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("id", id);
+        form.AddField("coin", coin);
+        form.AddField("passage", passge);
+        form.AddField("death", death);
+
+        WWW www = new WWW("GameDB.local", form);
+        yield return www;
+    }
+
+}
