63d297d72901920113f0cb01719c838ed299ad1c
ent1k1337 <ent1k1337@yandex.ru>
2020-12-09 17:17:42 +0300

    Добавлены окна Options, паузы и конца уровня, но не запрограммированы

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -7,15 +7,24 @@ public class Menu : MonoBehaviour
 {
     public GameObject canvas1;
     public GameObject canvas2;
+    public GameObject canvas3;
     public void LevelsWindow()
     {
         canvas1.SetActive(false);
         canvas2.SetActive(true);
+        canvas3.SetActive(false);
     }
     public void MainWindow()
     {
         canvas1.SetActive(true);
         canvas2.SetActive(false);
+        canvas3.SetActive(false);
+    }
+    public void OptionsWindow()
+    {
+        canvas1.SetActive(false);
+        canvas2.SetActive(false);
+        canvas3.SetActive(true);
     }
     public void ExitGame()
     {
